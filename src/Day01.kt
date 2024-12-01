fac37eda4ad446abca6cec9d1b023bcd523fbc7e
Antti Pekkanen <pekkanenantti@gmail.com>
2024-12-01 12:27:12 +0200

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,29 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: Pair<List<Int>, List<Int>>): Int {
+        return input.first.zip(input.second).sumOf { abs(it.first - it.second) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: Pair<List<Int>, List<Int>>): Int {
+        return input.first.fold(0) { sum, element -> sum + element * input.second.count { it == element } }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
+        val list1 = input.map { it.split("   ") }
+            .map { it[0].toInt() }
+            .sorted()
+        val list2 = input.map { it.split("   ") }
+            .map { it[1].toInt() }
+            .sorted()
+        return Pair(list1, list2)
+    }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = parseInput(readInput("Day01_test"))
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = parseInput(readInput("Day01"))
     part1(input).println()
     part2(input).println()
 }
